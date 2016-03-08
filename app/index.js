b0045971117c9b0384a98fe00b4d9c577539e70e
James Little <james@littlecode.co.uk>
2016-03-08 13:59:08 +0000

    second commit

--- app/index.js
+++ app/index.js
@@ -2,24 +2,29 @@ var USER_DATA = {
     name: 'James Little',
     username: 'jeeves64',
     image: 'https://avatars0.githubusercontent.com/u/1648681?v=3&s=460'
-}
+};
 
 var React = require('react');
 var ReactDOM = require('react-dom');
 
 var ProfilePic = React.createClass({
     render: function () {
-        return <img src={this.props.imageUrl} style={{height: 100, width: 100}} />
+        return <img src={this.props.imageUrl} style={{height: 100, width: 100}} />;
     }
 });
 
 var Link = React.createClass({
+    changeURL: function () {
+        window.location.replace(this.props.href);
+    },
     render: function () {
         return (
-            <span>
+            <span 
+                style={{color: 'blue', cursor: 'pointer'}}
+                onClick={this.changeURL}>
                 {this.props.children}
             </span>
-        )
+        );
     }
 });
 
@@ -31,7 +36,7 @@ var ProfileLink = React.createClass({
                     {this.props.username}
                 </Link>
             </div>
-        )
+        );
     }
 });
 
@@ -39,7 +44,7 @@ var ProfileName = React.createClass({
     render: function () {
         return (
             <div>{this.props.name}</div>
-        )
+        );
     }
 });
 
@@ -51,7 +56,7 @@ var Avatar = React.createClass({
                 <ProfileName name={this.props.user.name} />
                 <ProfileLink username={this.props.user.username} />
             </div>
-        )
+        );
     }
 });
 
